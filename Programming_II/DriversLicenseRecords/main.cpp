62ebec9a194bb4f04ad8d04c4552bbb0bdf3b385
Brandon (Archer) Lammey <archerlammey@ku.edu>
2016-09-17 22:52:41 -0500

    Changes

--- Programming_II/DriversLicenseRecords/main.cpp
+++ Programming_II/DriversLicenseRecords/main.cpp
@@ -5,7 +5,7 @@
  * Email Address: archerlammey@ku.edu 
  * Homework: Lab 02
  * Description: Creates an executive object which is a dynamically allocated array of objects.
- * Then the run function is used to run the program allowing a user to search records based on 3 criteria. 3
+ * Then the run function is used to run the program allowing a user to search records based on 3 criteria. 
  */
 
 #include "Executive.h"
